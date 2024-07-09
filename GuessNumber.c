f13d8f5b63a6565376c318f1d6c6102661b2bf39
ahmxdoo <ahmad.daoud1999@icloud.com>
2024-07-09 11:17:42 +0200

    C Learning

---
+++ GuessNumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int number, guess, tries = 0;
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    printf("Guess the number(between 1 and 100):\n");
+
+    do {
+        scanf("%d", &guess);
+        tries++;
+        if (guess > number){
+            printf("Too high!\n");
+        } else if (guess < number) {
+            printf("Too low\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d tries.\n");
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
